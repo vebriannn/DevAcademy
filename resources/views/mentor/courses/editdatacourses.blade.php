2ea9e88d606bf530be6c403b73c72c8f4ce3b5f5
YnCodess <vebrianns01@gmail.com>
2024-08-06 15:16:11 +0700

    commit by vebrian, create auth admin, course, lesson, chapter

--- resources/views/mentor/courses/editdatacourses.blade.php
+++ resources/views/mentor/courses/editdatacourses.blade.php
@@ -3,59 +3,102 @@
 @section('title, editdatacourse')
 
 @section('content-editdatacourses')
-<div class="container mt-3 mb-3 p-5">
-    <h2>Edit Data</h2>
-    <div class="row mt-5">
-        <img id="preview" src="https://via.placeholder.com/150" alt="Image" class="img-fluid col-4 py-2">
-        <form class="col-8" action="/update-data" method="post" enctype="multipart/form-data">
-            <div class="row">
-                <div class="col-6">
-                    <div class="entryarea">
-                        <select id="category" name="category" required>
-                            <option value="" disabled selected>Kategori</option>
-                            <option value="kategori1">Kategori 1</option>
-                            <option value="kategori2">Kategori 2</option>
-                            <option value="kategori3">Kategori 3</option>
-                        </select>
+    <div class="container mt-3 mb-3 p-5">
+        <h2>Edit Data</h2>
+        <div class="row mt-5">
+            <img id="preview" src="{{ $courses->cover }}" alt="Image" class="img-fluid col-4 py-2">
+            <form class="col-8" action="{{ route('admin.course.edit.update', $courses->id) }}" method="post" enctype="multipart/form-data">
+                <div class="row mt-5">
+                    @csrf
+                    @method('put')
+                    <div class="row">
+                        <div class="col-6">
+                            <div class="entryarea">
+                                <select id="category" name="category" required>
+                                    @foreach ($category as $item)
+                                        <option value="{{ $item->name }}"
+                                            {{ $courses->category == $item->name ? 'selected' : '' }}>
+                                            {{ $item->name }}
+                                        </option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-6">
+                            <div class="entryarea">
+                                <input type="text" id="title" name="name" placeholder=" " required
+                                    value="{{ $courses->name }}">
+                                <div class="labelline" for="title">title</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row mt-3">
+                        <div class="col-6">
+                            <div class="entryarea">
+                                <input type="text" id="link" name="price" placeholder=" " required
+                                    value="{{ $courses->price }}">
+                                <div class="labelline" for="link">price</div>
+                            </div>
+                        </div>
+                        <div class="col-6">
+                            <div class="col-6">
+                                <div class="entryarea">
+                                    <select id="category" name="status" required>
+                                        <option value="draft" {{ $courses->status == 'draft' ? 'selected' : '' }}>draft
+                                        </option>
+                                        <option value="published" {{ $courses->status == 'published' ? 'selected' : '' }}>
+                                            published</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-6">
+                            <div class="entryarea">
+                                <select id="category" name="level" required>
+                                    <option value="beginner" {{ $courses->level == 'beginner' ? 'selected' : '' }}>Beginner
+                                    </option>
+                                    <option value="intermediate" {{ $courses->level == 'intermediate' ? 'selected' : '' }}>
+                                        Intermediate</option>
+                                    <option value="expert" {{ $courses->level == 'expert' ? 'selected' : '' }}>Expert
+                                    </option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-6">
+                            <div class="entryarea">
+                                <input type="file" id="image" name="cover" accept="image/*">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row mt-3">
+                        <div class="col-12">
+                            <div class="textarea">
+                                <textarea id="description" name="description" placeholder=" " required>{{ $courses->description }}
+                                    </textarea>
+                                <div class="labelline-textarea" for="description">Description</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row mt-3">
+                        <di class="col-12">
+                            <div class="entryarea">
+                                <select id="category" name="type" required>
+                                    <option value="free" {{ $courses->type == 'free' ? 'selected' : '' }}>Free</option>
+                                    <option value="premium" {{ $courses->type == 'premium' ? 'selected' : '' }}>Premium
+                                    </option>
+                                </select>
+                            </div>
                     </div>
                 </div>
-                <div class="col-6">
-                    <div class="entryarea">
-                        <input type="number" id="title" name="title" placeholder=" " required>
-                        <div class="labelline" for="title">Title</div>
+                <div class="row mt-3">
+                    <div class="col-2">
+                        <button class="btn-kirim text-decoration-none" type="submit">Simpan</button>
+                    </div>
+                    <div class="col-2">
+                        <a class="btn-batal text-decoration-none" href="#">Batalkan</a>
                     </div>
                 </div>
-            </div>
-            <div class="row mt-3">
-                <div class="col-6">
-                    <div class="entryarea">
-                        <input type="text" id="link" name="link" placeholder=" " required>
-                        <div class="labelline" for="link">Link</div>
-                    </div>
-                </div>
-                <div class="col-6">
-                    <div class="entryarea">
-                        <input type="file" id="image" name="image" accept="image/*" required>
-                    </div>
-                </div>
-            </div>
-            <div class="row mt-3">
-                <div class="col-12">
-                    <div class="textarea">
-                        <textarea id="description" name="description" placeholder=" " required></textarea>
-                        <div class="labelline-textarea" for="description">Description</div>
-                    </div>
-                </div>
-            </div>
-            <div class="row mt-3">
-                <div class="col-2">
-                    <a class="btn-kirim text-decoration-none" href="#">Simpan</a>
-                </div>
-                <div class="col-2">
-                    <a class="btn-batal text-decoration-none" href="#">Batalkan</a>
-                </div>
-            </div>
-        </form>
+            </form>
+        </div>
     </div>
-</div>
 @endsection
--- resources/views/mentor/courses/tambahdatacourses.blade.php
+++ resources/views/mentor/courses/tambahdatacourses.blade.php
@@ -3,59 +3,89 @@
 @section('title, tambahdatacourse')
 
 @section('content-tambahdatacourses')
-<div class="container mt-3 mb-3 p-5">
-    <h2>Tambah Data</h2>
-    <div class="row mt-5">
-        <img id="preview" src="https://via.placeholder.com/150" alt="Image" class="img-fluid col-4 py-2">
-        <form class="col-8" action="/update-data" method="post" enctype="multipart/form-data">
-            <div class="row">
-                <div class="col-6">
-                    <div class="entryarea">
-                        <select id="category" name="category" required>
-                            <option value="" disabled selected>Kategori</option>
-                            <option value="kategori1">Kategori 1</option>
-                            <option value="kategori2">Kategori 2</option>
-                            <option value="kategori3">Kategori 3</option>
-                        </select>
+    <div class="container mt-3 mb-3 p-5">
+        <h2>Tambah Data</h2>
+        <div class="row mt-5">
+            <img id="preview" src="https://via.placeholder.com/150" alt="Image" class="img-fluid col-4 py-2">
+            <form class="col-8" action="{{ route('admin.course.create.store') }}" method="post" enctype="multipart/form-data">
+                @csrf
+                <div class="row">
+                    <div class="col-6">
+                        <div class="entryarea">
+                            <select id="category" name="category" required>
+                                @foreach ($category as $item)
+                                    <option value="{{ $item->name }}">{{ $item->name }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                    </div>
+                    <div class="col-6">
+                        <div class="entryarea">
+                            <input type="text" id="title" name="name" placeholder=" " required>
+                            <div class="labelline" for="title">Title</div>
+                        </div>
                     </div>
                 </div>
-                <div class="col-6">
-                    <div class="entryarea">
-                        <input type="number" id="title" name="title" placeholder=" " required>
-                        <div class="labelline" for="title">Title</div>
+                <div class="row mt-3">
+                    <div class="col-6">
+                        <div class="entryarea">
+                            <input type="text" id="link" name="price" placeholder=" " required>
+                            <div class="labelline" for="link">Price</div>
+                        </div>
+                    </div>
+                    <div class="col-6">
+                        <div class="col-6">
+                            <div class="entryarea">
+                                <select id="category" name="status" required>
+                                    <option value="draft">draft</option>
+                                    <option value="published">published</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-6">
+                        <div class="entryarea">
+                            <select id="category" name="level" required>
+                                <option value="beginner">Beginner</option>
+                                <option value="intermediate">Intermediate</option>
+                                <option value="expert">Expert</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="col-6">
+                        <div class="entryarea">
+                            <input type="file" id="image" name="cover" accept="image/*" required>
+                        </div>
                     </div>
                 </div>
+                <div class="row mt-3">
+                    <div class="col-12">
+                        <div class="textarea">
+                            <textarea id="description" name="description" placeholder=" " required></textarea>
+                            <div class="labelline-textarea" for="description">Description</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row mt-3">
+                    <di class="col-12">
+                        <div class="entryarea">
+                            <select id="category" name="type" required>
+                                <option value="free">Free</option>
+                                <option value="premium">Premium</option>
+                            </select>
+                        </div>
+
+                </div>
+        </div>
+        <div class="row mt-3">
+            <div class="col-2">
+                <button class="btn-kirim text-decoration-none" type="submit">Simpan</button>
             </div>
-            <div class="row mt-3">
-                <div class="col-6">
-                    <div class="entryarea">
-                        <input type="text" id="link" name="link" placeholder=" " required>
-                        <div class="labelline" for="link">Link</div>
-                    </div>
-                </div>
-                <div class="col-6">
-                    <div class="entryarea">
-                        <input type="file" id="image" name="image" accept="image/*" required>
-                    </div>
-                </div>
+            <div class="col-2">
+                <a class="btn-batal text-decoration-none" href="#">Batalkan</a>
             </div>
-            <div class="row mt-3">
-                <div class="col-12">
-                    <div class="textarea">
-                        <textarea id="description" name="description" placeholder=" " required></textarea>
-                        <div class="labelline-textarea" for="description">Description</div>
-                    </div>
-                </div>
-            </div>
-            <div class="row mt-3">
-                <div class="col-2">
-                    <a class="btn-kirim text-decoration-none" href="#">Simpan</a>
-                </div>
-                <div class="col-2">
-                    <a class="btn-batal text-decoration-none" href="#">Batalkan</a>
-                </div>
-            </div>
+        </div>
         </form>
     </div>
-</div>
+    </div>
 @endsection
