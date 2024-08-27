1b308096c5a1bed1c6fa8eab14f5881fe93b8563
priass <ikhlas.sambada@gmail.com>
2024-08-27 11:31:29 +0700

    fix landing page

--- resources/views/components/includes/member/footer.blade.php
+++ resources/views/components/includes/member/footer.blade.php
@@ -3,16 +3,16 @@
 @endpush --}}
 
 <!-- FOOTER -->
-<footer class="footer">
+<footer class="footer position-relative bottom-0">
     <div class="container py-4">
-        <div class="row align-items-center">
-            <div class="col-4  text-md-left logo">
+        <div class="row align-items-center text-center text-md-start gap-2 gap-md-0">
+            <div class="col-12 col-md-4 logo">
                 <img src="{{ asset('nemolab/admin/img/Logo Nemolab.png')}}" alt="Company Logo">
             </div>
-            <div class="col-4 text-center company-name">
-                <p class="my-auto fw-semibold">© 2024 Nemolab. All rights reserved.</p>
+            <div class="col-12 col-md-4 company-name text-md-center">
+                <p class="my-auto fw-semibold">© 2024 Nemolab. <br class="d-none d-md-block d-lg-none"> All rights reserved.</p>
             </div>
-            <div class="col-4 float-end text-right social-icons grid gap-lg-3 gap-md-0">
+            <div class="col-12 col-md-4 text-center text-md-end social-icons grid gap-lg-3 gap-md-0">
                 <a href="#"><img src="{{ asset('nemolab/admin/img/facebook.png')}}" alt=""></a>
                 <a href="#"><img src="{{ asset('nemolab/admin/img/twitter.png')}}" alt=""></a>
                 <a href="#"><img src="{{ asset('nemolab/admin/img/linkedin.png')}}" alt=""></a>
