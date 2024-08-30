2659f308eee95ec36f54a5e54638c047667d6e0b
Strioo <hapeb0566@gmail.com>
2024-08-30 14:06:28 +0700

    forum diskusi dan review

--- resources/views/components/layouts/member/navback.blade.php
+++ resources/views/components/layouts/member/navback.blade.php
@@ -36,7 +36,7 @@
     @stack('prepend-script')
     {{-- <script src="{{ asset('nemolab/assets/js/profile-navbar.js') }}"></script> --}}
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-    @stack('addon-style')
+    @stack('addon-script')
 </body>
 
 </html>
