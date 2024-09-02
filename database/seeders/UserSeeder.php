0150dc8769bfddf0b7f60851e68a192ed200ab6f
YnCodess <vebrianns01@gmail.com>
2024-09-02 14:08:39 +0700

    Resolved merge conflicts

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,7 +5,6 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
-use Faker\Factory as Faker;
 
 class UserSeeder extends Seeder
 {
