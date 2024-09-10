9dc891662a574c71b8de3a97c0138945ab9c431a
YnCodess <vebrianns01@gmail.com>
2024-09-10 12:34:44 +0700

    fix admin

--- app/Http/Controllers/Admin/AdminCourseController.php
+++ app/Http/Controllers/Admin/AdminCourseController.php
@@ -26,7 +26,7 @@ class AdminCourseController extends Controller
     {
         $user = Auth::user();
         $perPage = $request->input('per_page', 10);
-        if ($user->role === 'superadmin') {
+        if ($user->role == 'superadmin') {
             $courses = Course::with('users')->paginate($perPage);
         } else {
             $courses = Course::where('mentor_id', $user->id)->paginate($perPage);
