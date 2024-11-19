8de125e34a4ffe3982e22ec61433f1214715bd53
YnCodess <vebrianns01@gmail.com>
2024-11-19 11:54:30 +0700

    add ebook

---
+++ app/Models/Ebook.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Ebook extends Model
+{
+    use HasFactory;
+
+    protected $table = 'tbl_ebooks';
+
+    protected $fillable = [
+        'cover',
+        'category',
+        'name',
+        'type',
+        'product_type',
+        'status',
+        'level',
+        'price',
+        'description',
+        'file_ebook',
+        'mentor_id',
+        'slug'
+    ];
+
+    public static function generateUniqueSlug($name)
+    {
+        $slug = Str::slug($name);
+        $count = static::where('slug', 'LIKE', "{$slug}%")->count();
+
+        return $count ? "{$slug}-{$count}" : $slug;
+    }
+
+    public function users()
+    {
+        return $this->belongsTo(User::class, 'mentor_id', 'id');
+    }
+    public function transactions()
+    {
+        return $this->hasMany(Transaction::class);
+    }
+    // In Ebook.php model
+    public function courseEbooks()
+    {
+        return $this->hasMany(CourseEbook::class, 'ebook_id');
+    }
+}
